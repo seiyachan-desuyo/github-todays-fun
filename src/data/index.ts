f1e3131698662c2348ce08e4a679de72af1ee9e9
Aime <aime@localhost>
2026-09-18 13:47:18 +0800

    chore(edition): publish 2026-09-18 issue 10 (30 featured + 103 candidates)
    Co-Authored-By: Aime <aime@bytedance.com>

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,13 +1,14 @@
 import type { CandidatePool, CandidateProject, DailyEdition } from "@/lib/types";
-import edition0 from "./editions/2026-09-17.json";
-import edition1 from "./editions/2026-09-16.json";
-import edition2 from "./editions/2026-09-15.json";
-import edition3 from "./editions/2026-09-14.json";
-import edition4 from "./editions/2026-09-11.json";
-import edition5 from "./editions/2026-09-10.json";
-import edition6 from "./editions/2026-09-09.json";
-import edition7 from "./editions/2026-09-07.json";
-import edition8 from "./editions/2026-09-06.json";
+import edition0 from "./editions/2026-09-18.json";
+import edition1 from "./editions/2026-09-17.json";
+import edition2 from "./editions/2026-09-16.json";
+import edition3 from "./editions/2026-09-15.json";
+import edition4 from "./editions/2026-09-14.json";
+import edition5 from "./editions/2026-09-11.json";
+import edition6 from "./editions/2026-09-10.json";
+import edition7 from "./editions/2026-09-09.json";
+import edition8 from "./editions/2026-09-07.json";
+import edition9 from "./editions/2026-09-06.json";
 import enriched0 from "./enriched-candidates/2026-09-09.json";
 import enriched1 from "./enriched-candidates/2026-09-10.json";
 import enriched2 from "./enriched-candidates/2026-09-11.json";
@@ -15,11 +16,12 @@ import enriched3 from "./enriched-candidates/2026-09-14.json";
 import enriched4 from "./enriched-candidates/2026-09-15.json";
 import enriched5 from "./enriched-candidates/2026-09-16.json";
 import enriched6 from "./enriched-candidates/2026-09-17.json";
+import enriched7 from "./enriched-candidates/2026-09-18.json";
 
-export const editions = [edition0, edition1, edition2, edition3, edition4, edition5, edition6, edition7, edition8] as DailyEdition[];
+export const editions = [edition0, edition1, edition2, edition3, edition4, edition5, edition6, edition7, edition8, edition9] as DailyEdition[];
 export const latestEdition = editions[0];
 
-export const candidatePools: CandidatePool[] = [{ date: "2026-09-09", projects: enriched0.projects as CandidateProject[] }, { date: "2026-09-10", projects: enriched1.projects as CandidateProject[] }, { date: "2026-09-11", projects: enriched2.projects as CandidateProject[] }, { date: "2026-09-14", projects: enriched3.projects as CandidateProject[] }, { date: "2026-09-15", projects: enriched4.projects as CandidateProject[] }, { date: "2026-09-16", projects: enriched5.projects as CandidateProject[] }, { date: "2026-09-17", projects: enriched6.projects as CandidateProject[] }];
+export const candidatePools: CandidatePool[] = [{ date: "2026-09-09", projects: enriched0.projects as CandidateProject[] }, { date: "2026-09-10", projects: enriched1.projects as CandidateProject[] }, { date: "2026-09-11", projects: enriched2.projects as CandidateProject[] }, { date: "2026-09-14", projects: enriched3.projects as CandidateProject[] }, { date: "2026-09-15", projects: enriched4.projects as CandidateProject[] }, { date: "2026-09-16", projects: enriched5.projects as CandidateProject[] }, { date: "2026-09-17", projects: enriched6.projects as CandidateProject[] }, { date: "2026-09-18", projects: enriched7.projects as CandidateProject[] }];
 
 export function getEdition(date: string): DailyEdition | undefined {
   return editions.find((edition) => edition.date === date);
